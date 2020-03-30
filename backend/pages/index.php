94c540f9664a17bafde047422eb0dea9f4eaf6f1
upss1988 <upss070288@gmail.com>
2020-03-31 00:21:54 +0200

    Added structure for Dashboard home page

--- backend/pages/index.php
+++ backend/pages/index.php
@@ -10,7 +10,30 @@
     <div id="layoutSidenav_content">
         <main>
             <!-- Here add the code -->
+            <div class="container-fluid">
+                <h4 class="mt-4"><?php echo 'Welcome back '.$_SESSION['fname']; ?></h4>
+                <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Home</li>
+                </ol>
 
+                <div class="card mb-4">
+                    <div class="card-body">You can manage your data here.</div>
+                </div>
+
+                <!-- Profile Information -->
+                <div class="card mb-4">
+                    <div class="card-header"><i class="fas fa-table mr-1"></i>Information</div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            Add data here
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
 
 
 
