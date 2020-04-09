4d4b5bcd5938c16260c860d4c3c388982d999f61
Marko Radulovic <upss070288@gmail.com>
2020-04-10 00:28:11 +0200

    Fixed user role message on navigation tab

--- backend/src/templates/auth/users/user_name.php
+++ backend/src/templates/auth/users/user_name.php
@@ -1,4 +1,5 @@
 <div class="sb-sidenav-footer">
     <div class="small">Welcome back: <?php echo $_SESSION['fname'].' '.$_SESSION['lname']; ?></div>
-    <div class="small">Member since: <?php echo strstr(str_replace('-', '/', $_SESSION['date']), ' ', true); ?></div>
+<!--    <div class="small">Member since: --><?php //echo strstr(str_replace('-', '/', $_SESSION['date']), ' ', true); ?><!--</div>-->
+    <div class="small">User role: <?= $isAdmin = ($_SESSION['user_role'] === 10) ? 'Admin' : 'Customer'; ?></div>
 </div>
